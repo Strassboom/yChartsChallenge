b0e2df355fd611dfa57652abe734db946d7e614c
Strassboom <maddog241996@gmail.com>
2021-03-24 23:57:51 -0400

    Fixed program so it writes to recon.out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#def dayParser(lines,dayName,index):
-    
-
 def daySeparator(filepath):
     with open(filepath,'r') as r:
+        # lines will help us keep track of when we're moving to the next record set
         lines = [x.strip() for x in r.readlines()]
         i = 0
         firstDay = lines[i]
@@ -39,21 +37,24 @@ def daySeparator(filepath):
                     days[firstDay]['Cash'] += float(totalValue)
             i += 1
         i += 2
+        reconOut = ''
         while i < len(lines):
             stockRecord = lines[i].split(' ')
             symbol,shares = stockRecord
             try:
                 if float(shares) != days[firstDay][symbol]:
-                    print(symbol,float(shares) - days[firstDay][symbol])
+                    nextLine = f"{symbol} {float(shares) - days[firstDay][symbol]}\n"
+                    reconOut += nextLine
                 days[firstDay].pop(symbol)
             except KeyError:
                 if symbol not in days[firstDay].keys():
-                    print(symbol,shares)
+                    reconOut += f"{symbol} {float(shares)}\n"
             i += 1
         for item in days[firstDay].items():
             if item[1] != 0:
-                print(item[0],item[1])
-        #print(lines)
+                reconOut += f"{item[0]} {item[1]}\n"
+        with open('recon.out','w+') as w:
+            w.write(reconOut)
 
 def main():
     daySeparator('data.txt')
